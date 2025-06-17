caa1ebc499b31f0a493c74a61d71427610496f14
sreejareddy624 <245122749010@mvsrec.edu.in>
2025-06-17 15:12:40 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+public class Demo1{
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=0;i<=10;i++)
+		{
+			sum+=i;
+		}
+		double avg=sum/10;
+		System.out.println("sum:"+sum);
+		System.out.println("avg:"+avg);
+	}
+}
